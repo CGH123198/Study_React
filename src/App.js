8e02a06fccdb3ed98b7da0f00d0802aedac62078
CGH <tt878429@gmail.com>
2022-06-05 02:19:44 +0900

    06/05

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
-import Say from './Say.js';
-import Counter from './Counter.js';
+import EventPractice from './practice/EventPractice'
 import './App.css';
 
-class MyComponent extends Component {
-  render() {
-    const { name, favoriteNumber, children } = this.props;
-    return (
-      <div>
-        Hello, My name is {name}. <br />
-        The value of children is {children}. <br />
-        My favoriteNumber is {favoriteNumber}.
-      </div>
-    );
-  }
-}
-
-MyComponent.defaultProps = {
-  name: '기본 이름',
-  favoriteNumber: 11
-}
-
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired
-};
 
 function App() {
   return (
     <div className="App">
-      <MyComponent>리액트</MyComponent>
-      <Counter />
-      <Say />
+      <EventPractice />
     </div>
   );
 }
