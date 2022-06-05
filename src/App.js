7d81a57f1f225444345337b3e079b8f41bebe82b
CGH <tt878429@gmail.com>
2022-06-05 15:13:23 +0900

    06/05

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
-import EventPractice from './practice/EventPractice'
+import React, { Component } from 'react';
+import ScrollBox from './practice/ScrollBox.js'
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <EventPractice />
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+      <div className="App">
+        <ScrollBox ref={ (ref) => this.scrollBox=ref }/>
+        <button onClick={ () => this.scrollBox.scrollToBottom() }>맨 밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
