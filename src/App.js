96f313bf61bef1869ca0beba43d2f2b56a27f7da
CGH <tt878429@gmail.com>
2022-06-07 16:07:26 +0900

    06/07

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import ScrollBox from './practice/ScrollBox.js'
+import LifeCycleSample from './practice/LifeCycleSample.js';
+import ErrorBoundary from './practice/ErrorBoundary.js';
 import './App.css';
+
+function getRandomColor() {
+   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 
 class App extends Component {
+  state ={
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
     render() {
       return (
       <div className="App">
-        <ScrollBox ref={ (ref) => this.scrollBox=ref }/>
-        <button onClick={ () => this.scrollBox.scrollToBottom() }>맨 밑으로</button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }
