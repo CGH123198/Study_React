b7e29f6cde9ca64e7a39e2ddd2c569aae4819b74
CGH <tt878429@gmail.com>
2022-06-08 12:53:36 +0900

    06/07~08

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState } from 'react';
+import Info from './Info_R';
+
+const App = () => {
+  return(
+    <div>
+      <Info />
+    </div>
+  );
+}
+export default App;
