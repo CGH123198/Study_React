218a1b7c8158d9cce71c9ffda9ba89f0c3285e07
CGH <tt878429@gmail.com>
2022-06-17 22:59:49 +0900

    06/17

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,21 @@
+import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({number, increase, decrease}) => {
+    return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+};
+
+export default connect(
+    state => ({
+        number: state.counter.number
+    }),
+    dispatch => bindActionCreators(
+        {
+            increase,
+            decrease,
+        }, 
+        dispatch,
+    ),
+)(CounterContainer);
