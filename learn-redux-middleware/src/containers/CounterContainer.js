90841278674ac3616c338ff144d13a7d9e06192f
CGH <tt878429@gmail.com>
2022-06-20 20:52:51 +0900

    06/20

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,16 +1,17 @@
 import { useSelector, useDispatch } from "react-redux/es/exports";
-import { increase, decrease } from '../modules/counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 import Counter from '../components/Counter';
 
 const CounterContainer = () => {
     const number = useSelector(state => state.counter);
     const dispatch = useDispatch();
 
+
     return (
         <Counter 
             number={number} 
-            onIncrease={() => dispatch(increase())} 
-            onDecrease={() => dispatch(decrease())} 
+            onIncrease={() => dispatch(increaseAsync())} 
+            onDecrease={() => dispatch(decreaseAsync())} 
         />
     );
 };
---
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -0,0 +1,35 @@
+import Sample from '../components/Sample';
+import { getPost, getUsers} from '../modules/sample';
+import React, { useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const SampleContainer = (
+) => {
+    const post = useSelector(state => state.sample.post);
+    const users = useSelector(state => state.sample.users);
+    const loadingPost = useSelector(state => state.sample.loading.GET_POST);
+    const loadingUser = useSelector(state => state.sample.loading.GET_USERS);
+
+    const dispatch = useDispatch();
+    const dgetPost = useCallback( id => {
+        dispatch(getPost(id));
+    }, [getPost]);
+    const dgetUsers = useCallback( id => {
+        dispatch(getUsers(id));
+    }, [dispatch]);
+    useEffect( () => {
+        dgetPost(1);
+        dgetUsers(1);
+    }, [dgetPost, dgetUsers]);
+
+    return (
+        <Sample
+            post={post}
+            users={users}
+            loadingPost={loadingPost}
+            loadingUser={loadingUser}
+        />
+    );
+};
+
+export default SampleContainer;
