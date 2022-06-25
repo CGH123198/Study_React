612d30200ea869f0e831faff8d8ec259c64caa42
CGH <tt878429@gmail.com>
2022-06-25 15:23:31 +0900

    06/25 koa REST API

---
+++ blog/blog-backend/src/api/posts/index.js
@@ -0,0 +1,13 @@
+const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
+
+const posts = new Router();
+
+posts.get('/', postsCtrl.list);
+posts.get('/:id', postsCtrl.read);
+posts.post('/', postsCtrl.write);
+posts.delete('/:id', postsCtrl.remove);
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
+
+module.exports = posts;
