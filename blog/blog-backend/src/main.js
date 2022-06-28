1e3b301f6555ed533b141ac643082e74fb05803a
CGH <tt878429@gmail.com>
2022-06-28 14:33:33 +0900

    06/28

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -4,7 +4,7 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
-import createFakeData from './createFakeData';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -23,6 +23,8 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
+
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
