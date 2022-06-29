968b38c7209ed62ec9cebb8aaf6efa48564eb89b
CGH <tt878429@gmail.com>
2022-06-29 15:15:22 +0900

    06/29

---
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const AuthFormBlock = styled.div`
+
+`;
+
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthFrom
+        </AuthFormBlock>
+    );
+};
+
+export default AuthForm;
